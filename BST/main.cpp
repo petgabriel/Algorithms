d77bf9f0e4b76cb636db1ca5091a28649881d62f
petgabriel <53994895+petgabriel@users.noreply.github.com>
2019-08-13 20:30:43 +0200

    Update main.cpp

--- BST/main.cpp
+++ BST/main.cpp
@@ -20,24 +20,61 @@ int main(int argc, char* argv[])
 
 	cout << endl << "- - - - - - - - - - - - - - - - - - - - - - - - -" << endl;
 
-	cout << "minimum: " << T.tree_minimum()->key << endl;
-	cout << "maximum: " << T.tree_maximum()->key << endl;
+	cout << "Minimum: " << T.tree_minimum(T.root)->key << endl;
+	cout << "Maximum: " << T.tree_maximum(T.root)->key << endl << endl;
+
+	cout << "Height of tree: " << T.tree_height(T.root) << endl << endl;
+
+	cout << "Inorder walk: ";
+	T.inorder_walk(T.root);
+	cout << endl;
+
+	cout << "Preorder walk: ";
+	T.preorder_walk(T.root);
+	cout << endl;
+
+	cout << "Postorder walk: ";
+	T.postorder_walk(T.root);
+	cout << endl << endl;
 
 	int k = rand() % 1000 + 1;
 
-	cout << "searching for: " << k << " -> ";
+	cout << "Searching for: " << k << " -> ";
 
 	node* z = T.tree_search(T.root, k);
 
 	if (z != NULL)
 	{
-		cout << "found! " << endl;
+		cout << "Found! " << endl;
 	}
 	else
 	{
 		cout << "No node with this key!" << endl;
 	}
+
+	node* successor = T.tree_successor(T.root);
 	
+	if (successor != NULL)
+	{
+		cout << "Successor of root: " << successor->key << endl;
+	}
+
+	else
+	{
+		cout << "There is no successor of root!" << endl;
+	}
+
+	cout << endl << "Deleting root: " << endl;
+
+	/*T.tree_delete(T.root);
+
+	cout << endl << "- - - - - - - - - - - - - - - - - - - - - - - - -" << endl;
+
+	T.print_tree();
+
+	cout << endl << "- - - - - - - - - - - - - - - - - - - - - - - - -" << endl;*/
+
 	system("pause");
+
 	return 0;
 }
